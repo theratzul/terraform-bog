f894d3c966496aa0fd1295cdfb99ace4ad2953f7
Popa Bogdan Robert <mailtobogdan@icloud.com>
2024-01-30 21:15:15 +0200

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    access_key = "${var.accesskey}"
+    secret_key = " "
+    region = " "
+
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-ad493bce"
+    instance_type = "t2.micro"
+}
