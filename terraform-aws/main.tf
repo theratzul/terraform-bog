5e9e06e8f869e939ab719a61f68ff0cb52fdde45
Bogdan <bogdan@localhost.localdomain>
2024-03-27 22:36:43 +0200

    feautre/add-push-for-files-in-s3-bucket

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -2,11 +2,20 @@ provider "aws" {
     region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "sweeden-bucket" {
+resource "aws_s3_bucket" "bucket" {
     bucket = "theratzul2"
 
     tags   = {
         Name    = "Mybucket"
         Environment = "Production"
     }
+}
+
+resource "aws_s3_object" "provision_source_files" {
+    bucket = aws_s3_bucket.bucket.id
+
+    for_each = fileset("myapp/", "**/*.*")
+    key = each.value
+    source = "myapp/${each.value}"
+    content_type = each.value
 }
