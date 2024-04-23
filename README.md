7b6f3e4c35f703a54a2d795517cbab76d2d148cc
Bogdan <bogdan@localhost.localdomain>
2024-04-23 22:05:03 +0300

    feature/lifecycle-rules

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform-bog  
-terraform apply ./terraform-list/main.tf
+terraform apply ./terraform-list/main.tf  
+terraform validate || terraform show || terraform show --json || terraform refresh  || terraform graf || terraform init --upgrade  
+lifecycle: create_before_destroy = true || lifecycle prevent_destroy = true  
--- terraform-local-file/main.tf
+++ terraform-local-file/main.tf
@@ -1,10 +1,21 @@
 resource "local_file" "pet" {
     filename = "/home/bogdan/bogdan.txt"
     content = "We love pets"
+
+    # A new resource will be created before deleting the old one
+    lifecycle {
+    create_before_destroy = true    
+    }
+
 }
 
 resource "random_pet" "my-pet" {
     prefix = "Mr"
     separator = "."
     length = "1"
+
+    # The resource is not deleted at apply but is deleted at destroy
+    lifecycle {
+        prevent_destroy = true
+    }
 }
