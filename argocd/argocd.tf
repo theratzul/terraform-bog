73f7cc3836f2d27d1a930b899a6c6219007419b5
Bogdan <bogdan@localhost.localdomain>
2024-05-28 22:45:05 +0300

    fix/terraform-argocd

---
+++ argocd/argocd.tf
@@ -0,0 +1,13 @@
+# helm repo add argocd https://argoproj.github.io/argo-helm 
+
+resource "helm_release" "argocd" {
+    name = "argocd"
+
+    repository = "https://argoproj.github.io/argo-helm"
+    chart      = "argo-cd"
+    namespace  = "argocd"
+    create_namespace = true
+    version    = "3.35.4"
+
+    values = [file("values/argocd.yaml")]
+}
